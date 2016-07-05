d64e42b47472eca340490245f3fe2f7cf7bd2f6a
Sean Hoyt <seanhoyt963@gmail.com>
2016-07-05 14:49:19 -0700

    Update tabs-common-vendor-blobs.mk

--- tabs-common/tabs-common-vendor-blobs.mk
+++ tabs-common/tabs-common-vendor-blobs.mk
@@ -148,7 +148,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/tabs-common/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
     vendor/samsung/tabs-common/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
     vendor/samsung/tabs-common/proprietary/vendor/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
-    vendor/samsung/tabs-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so
+    vendor/samsung/tabs-common/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/samsung/gts2-common/proprietary/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so \
+    vendor/samsung/gts2-common/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so
 
 # SSWAP
 PRODUCT_COPY_FILES += \
